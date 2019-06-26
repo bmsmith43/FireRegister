2cea555c882c3b87b11eb0414d234b33c0f96d67
Mark Smith <MSmith@mitek.co.uk>
2019-06-26 11:18:02 +0100

    initial commit

---
+++ FireRegister/FireRegister/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FireRegister.Models;
+
+namespace FireRegister.ViewModels
+{
+   public class ItemDetailViewModel : BaseViewModel
+   {
+      public Item Item { get; set; }
+      public ItemDetailViewModel(Item item = null)
+      {
+         Title = item?.Text;
+         Item = item;
+      }
+   }
+}
